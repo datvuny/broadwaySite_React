c8c2d82d0fd8ddb482aca52f9b9ea0d13a0c6652
dat vu <91701889+datvuny@users.noreply.github.com>
2023-05-20 17:34:37 -0400

    add: html/css

--- jboot/broadway/js/app.js
+++ jboot/broadway/js/app.js
@@ -11,29 +11,9 @@ $(document).ready(function(){
         }
     })
 
-    const typed = new Typed('.element', {
-        strings: ["Dat Vu", "a Software Engineer"],
-        smartBackspace:true,
-        typeSpeed: 100,
-        backSpeed: 150,
-        loop:true,
-        loopCount: Infinity,
-        startDelay: 1000
-      })
+   
 
 
-      const waypoint = new Waypoint({
-        element: document.getElementById('exp-id'),
-        handler: function() {
-         const p = document.querySelectorAll('.progress-bar')
-      p[0].setAttribute('style','width:88%; transition:1s all')
-      p[1].setAttribute('style','width:88%; transition:1.5s all')
-      p[2].setAttribute('style','width:83%; transition:1.7s all')
-      p[3].setAttribute('style','width:60%; transition:2s all')
-      p[4].setAttribute('style','width:79%; transition:2.3s all') 
-        },
-        offset:'90%'
-      })
      
 
       
