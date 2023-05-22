06d1b0d2c676e39486db63a2b267edd3b375c83c
dat vu <91701889+datvuny@users.noreply.github.com>
2023-05-22 16:50:10 -0400

    add css/html

--- jboot/broadway/js/app.js
+++ jboot/broadway/js/app.js
@@ -9,12 +9,24 @@ $(document).ready(function(){
         } else {
             $('.sticky').removeClass('stickyadd')
         }
-    })
-
-   
-
-
-     
-
-      
+    })    
 })
+
+window.onload = function() {
+    let likeBtn = document.querySelector("#like-btn");
+    let showLikes = document.querySelector("#show-likes");
+    let likes = parseInt(localStorage.getItem("likes")) || 201;
+    showLikes.innerHTML = countLikes(likes);
+  
+    likeBtn.addEventListener("click", (e) => {
+      likes++; // Increment the likes counter by 1
+      showLikes.innerHTML = countLikes(likes);
+      localStorage.setItem("likes", likes); // Store the updated likes count in localStorage
+      console.log(countLikes(likes));
+    });
+  
+    function countLikes(likes) {
+        return likes;
+      }
+    }
+  
