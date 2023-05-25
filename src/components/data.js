c30a5837c6dd8bba6c1e36017947b91c6bd060fb
dat vu <91701889+datvuny@users.noreply.github.com>
2023-05-25 16:54:22 -0400

    edit css

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 export const sectionsData = [
     {
       title: 'Once Upon A One More Time',
-      imageUrl: './broadwaySite_React/images/onemoretime.jpeg',
+      imageUrl: './images/onemoretime.jpeg',
       address:'MARQUIS THEATRE\n210 W 46th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/210+W+46th+St,+New+York,+NY+10036/@40.7587408,-73.9889054,16.96z/data=!4m6!3m5!1s0x89c25854520e3895:0xaf5251e56c0a2839!8m2!3d40.7587252!4d-73.98622!16s%2Fg%2F11bw3xpqh2',
       website: 'https://www.onemoretimemusical.com',
@@ -9,7 +9,7 @@ export const sectionsData = [
     },
     {
       title: 'The Thanksgiving Play',
-      imageUrl: './broadwaySite_React/images/thanks.jpeg',
+      imageUrl: './images/thanks.jpeg',
       address:'HAYES THEATRE\n240 W 44th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/The+Hayes+Theater/@40.7579127,-73.9877214,15z/data=!4m2!3m1!1s0x0:0xc491922ebe2f2583?sa=X&ved=2ahUKEwicnqyJ-I7_AhV-F1kFHayiClcQ_BJ6BAhvEAg',
       website: 'https://www.2ST.com',
@@ -17,7 +17,7 @@ export const sectionsData = [
     },
     {
       title: 'The Cottage',
-      imageUrl: './broadwaySite_React/images/cot.jpeg',
+      imageUrl: './images/cot.jpeg',
       address:'HAYES THEATRE\n240 W 44th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/The+Hayes+Theater/@40.7579127,-73.9877214,15z/data=!4m2!3m1!1s0x0:0xc491922ebe2f2583?sa=X&ved=2ahUKEwicnqyJ-I7_AhV-F1kFHayiClcQ_BJ6BAhvEAg',
       website: 'https://thecottageonbroadway.com/',
@@ -25,7 +25,7 @@ export const sectionsData = [
     },
     {
       title: 'The Light In The Piazza',
-      imageUrl: './broadwaySite_React/images/dm.jpeg',
+      imageUrl: './images/dm.jpeg',
       address:'NYC CITY CENTER\n131 W 55th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/New+York+City+Center/@40.7639446,-73.9794401,15z/data=!4m6!3m5!1s0x89c258f9cfcb250d:0xdb570ddcb766e3a8!8m2!3d40.7639446!4d-73.9794401!16zL20vMDc4cWwz?entry=ttu',
       website: 'https://www.nycitycenter.org/pdps/2022-2023/light-in-the-piazza/',
@@ -33,7 +33,7 @@ export const sectionsData = [
     },
     {
       title: 'Kimberly Akimbo',
-      imageUrl: './broadwaySite_React/images/kim.jpeg',
+      imageUrl: './images/kim.jpeg',
       address:'BOOTH THEATRE\n222 W 45th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/Booth+Theatre/@40.7583924,-73.9870106,15z/data=!4m6!3m5!1s0x89c2585665784711:0x21635c43ec03681a!8m2!3d40.7583924!4d-73.9870106!16zL20vMGMwN3Rq?entry=ttu',
       website: 'https://kimberlyakimbothemusical.com/',
@@ -41,7 +41,7 @@ export const sectionsData = [
     },
     {
       title: 'The Lion King',
-      imageUrl: './broadwaySite_React/images/lion.png',
+      imageUrl: './images/lion.png',
       address:'MINSKOFF Theater\n200 W 45th St\nNY, NY 12345',
       location: 'https://www.google.com/maps/place/The+Lion+King/@40.7580227,-73.986025,15z/data=!4m2!3m1!1s0x0:0xea81775ec7e6560b?sa=X&ved=2ahUKEwjqmrHH-Y7_AhXtK1kFHVxhB4IQ_BJ6BAhqEAg',
       website: 'https://www.lionking.com',
@@ -49,7 +49,7 @@ export const sectionsData = [
     },
     {
       title: 'Shucked',
-      imageUrl: './broadwaySite_React/images/shu.jpeg',
+      imageUrl: './images/shu.jpeg',
       address:'NEDERLANDER THEATRE\n208 W 41st St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/Nederlander+Theatre/@40.7554996,-73.9882829,15z/data=!4m6!3m5!1s0x89c259ab527cf713:0x218dc118b3b9f832!8m2!3d40.7554996!4d-73.9882829!16zL20vMGMwN2g1?entry=ttu',
       website: 'https://shuckedmusical.com/',
@@ -57,7 +57,7 @@ export const sectionsData = [
     },
     {
       title: 'The Sign in Sidney Brustein\'s Window',
-      imageUrl: './broadwaySite_React/images/sid.jpeg',
+      imageUrl: './images/sid.jpeg',
       address:'JAMES EARL JONES THEATRE\n138 W 48th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/James+Earl+Jones+Theatre/@40.759197,-73.983008,15z/data=!4m6!3m5!1s0x89c25855f60976e9:0x9409d6da413e395c!8m2!3d40.759197!4d-73.983008!16zL20vMGMwODAx?hl=en&entry=ttu',
       website: 'https://thesignonbroadway.com/',
@@ -65,7 +65,7 @@ export const sectionsData = [
     },
     {
       title: 'Grey House',
-      imageUrl: './broadwaySite_React/images/grey.jpeg',
+      imageUrl: './images/grey.jpeg',
       address:'LYCEUM THEATRE\n149 W 44th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/Lyceum+Theatre/@40.7577485,-73.9846178,15z/data=!4m6!3m5!1s0x89c258550a61e8b5:0x638fc348d6cc418c!8m2!3d40.7577485!4d-73.9846178!16zL20vMGMwcnFr?entry=ttu',
       website: 'https://greyhousebroadway.com/',
@@ -73,7 +73,7 @@ export const sectionsData = [
     },
     {
       title: 'The Book of Mormon',
-      imageUrl: './broadwaySite_React/images/mor.jpeg',
+      imageUrl: './images/mor.jpeg',
       address:'EUGENE O’NEILL THEATRE\n230 W 49th St\nNY, NY 10019',
       location: 'https://www.google.com/maps/place/Eugene+O\'Neill+Theatre/@40.761133,-73.9878529,17z/data=!3m1!4b1!4m5!3m4!1s0x89c25856639258f7:0x6bd7c69bc1ae0216!8m2!3d40.761165!4d-73.9856217?shorturl=1',
       website: 'https://bookofmormonbroadway.com/',
@@ -81,7 +81,7 @@ export const sectionsData = [
     },
     {
       title: 'A Beautiful Noise',
-      imageUrl: './broadwaySite_React/images/neil.jpeg',
+      imageUrl: './images/neil.jpeg',
       address:'BROADHURST THEATRE\n235 W 44th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/Broadhurst+Theatre/@40.7581621,-73.987565,15z/data=!4m2!3m1!1s0x0:0xf4b57c88d094f352?sa=X&ved=2ahUKEwjApNrtsY__AhUDFlkFHXc2AHoQ_BJ6BAh1EAg',
       website: 'https://abeautifulnoisethemusical.com/',
@@ -89,7 +89,7 @@ export const sectionsData = [
     },
     {
       title: 'MJ The Musical',
-      imageUrl: './broadwaySite_React/images/mj.png',
+      imageUrl: './images/mj.png',
       address:'NEIL SIMON THEATRE\n250 W 52ND St\nNY, NY 10019',
       location: 'https://www.google.com/maps/place/Neil+Simon+Theatre/@40.7631784,-73.9845388,15z/data=!4m2!3m1!1s0x0:0xaa5dbb82ce9f27cb?sa=X&ved=2ahUKEwiqw9_8so__AhWwEFkFHSsrCFoQ_BJ6BAh3EAg',
       website: 'https://newyork.mjthemusical.com/',
@@ -97,7 +97,7 @@ export const sectionsData = [
     },
     {
       title: 'Parade',
-      imageUrl: './broadwaySite_React/images/parade.jpeg',
+      imageUrl: './images/parade.jpeg',
       address:'BERNARD B. JACOBS THEATRE\n242 W 45th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/242+W+45th+St,+New+York,+NY+10036/@40.7586707,-73.987736,17z/data=!3m1!4b1!4m6!3m5!1s0x89c258546420db91:0xebbe22510b4b9442!8m2!3d40.7586707!4d-73.987736!16s%2Fg%2F11bw3_fj2c?entry=ttu',
       website: 'https://paradebroadway.com/',
@@ -105,7 +105,7 @@ export const sectionsData = [
     },
     {
       title: 'Peter Pan Goes Wrong',
-      imageUrl: './broadwaySite_React/images/pete.png',
+      imageUrl: './images/pete.png',
       address:'BARRYMORE THEATRE\n243 W 47th St\nNY, NY 10036',
       location: 'https://www.google.com/maps/place/243+W+47th+St,+New+York,+NY+10036/data=!4m2!3m1!1s0x89c258542eaa4ddd:0x409de220ff77dd9b?sa=X&ved=2ahUKEwi43sq3tY__AhW-KlkFHSdgCTYQ8gF6BAgNEAI',
       website: 'https://pangoeswrongbway.com/',
